9e829098958a9671315369fefead181554b608f1
EXHAAS <60334101+EXHAAS@users.noreply.github.com>
2020-02-12 14:06:23 -0500

    Add files via upload

---
+++ BikePart.java
@@ -0,0 +1,119 @@
+/**
+*CPSC-240
+*I hereby declare upon my word of honor that I have neither given nor recieved
+*unauthorized help on this work.
+*@author Eleanor Haas
+*/
+
+public class BikePart{
+   private String name;
+   private long number;
+   private double listPrice;
+   private double salesPrice;
+   private boolean onSale;   
+   private int quantity;
+
+//USING THE DEFAULT CONSTRUCTOR
+
+/**
+*@param n The name to be set
+*/
+   public void setName(String n){
+      this.name = n;
+   }
+   
+/**
+*@return The BikePart name  
+*/
+   public String getName(){
+      return name;
+   }
+
+/**
+*@param n The BikePart number being set
+*/   
+   public void setNumber(long n){
+      this.number = n;
+   }
+   
+/**
+*@return the BikePart number
+*/   
+   public long getNumber(){
+      return number;
+   }
+
+/**
+*@param listPrice The listed price of the part
+*/   
+   public void setListPrice(double listPrice){
+      this.listPrice = listPrice;
+   }
+
+/**
+*@return The part's listed price
+*/   
+   public double getListPrice(){
+      return this.listPrice;
+   }
+ 
+/**
+*@param salesPrice The sales price of the part
+*/  
+   public void setSalesPrice(double salesPrice){
+      this.salesPrice = salesPrice;
+   }
+
+/**
+*@return The sales price of the part
+*/   
+   public double getSalesPrice(){
+      return this.salesPrice;
+   }
+
+/**
+*@param onSale If the part is on sale the value is true. If not it's false
+*/  
+   public void setOnSale(boolean onSale){
+      this.onSale = onSale;
+   }
+
+/**
+@return True if the part is on sale, false if not
+*/   
+   public boolean getOnSale(){
+      return this.onSale;
+   }
+   
+/**
+*@return The quantity of the BikePart in stock
+*/
+   public int getQuantity(){
+      return this.quantity;
+   }
+   
+/**
+*@param q The quantity to be set
+*/
+   public void setQuantity(int q){
+      this.quantity = q;
+   }
+
+/**
+*Prints all of the parts info, seperated by commas not spaces
+*/   
+   public void printInfo(){
+      System.out.println(name + "," + number + "," + listPrice + "," + salesPrice
+      + "," + onSale);
+   }
+
+/**
+*Returns the part's info as a String
+*/   
+   public String getInfo(){
+      return name + "," + number + "," + listPrice + "," + salesPrice + "," + onSale;
+   }
+}
+
+
+   

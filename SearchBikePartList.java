3fe1972f4b3e66886c71324ce075110589c26ccf
Anthony Tompkins <52788799+azt2504@users.noreply.github.com>
2020-02-14 02:41:32 -0500

    Update SearchBikePartList.java

--- SearchBikePartList.java
+++ SearchBikePartList.java
@@ -12,22 +12,22 @@ public class SearchBikePartList
 /**
  *
  *This is a method that takes a LinkedList of BikePart objects and a name corresponding to the name field of 
- *a BikePart in the given LinkedList. It iterates through the LinkedList and returns the BikePart whos's name field 
+ *a BikePart in the given LinkedList. It iterates through the LinkedList and returns the BikePart whos's part number field 
  *is equal to the given string.
  *
  * @param bPLinkedList This parameter is the LinkedList that is being searched through.
- * @param bPName This parameter is the name of the BikePart that is being looked for.
+ * @param bPNumber This parameter is the part number of the BikePart that is being looked for.
  * @return Returns BikePart named "part".
  */
-	public static BikePart searchBikePartList(LinkedList<BikePart> bPLinkedList, String bPName)
+	public static BikePart searchBikePartList(LinkedList<BikePart> bPLinkedList, String bPNumber)
 	{
 		BikePart part = null;
  		
 		for (BikePart currentPart : bPLinkedList) 
 		
 		{
-			String currentPartName = currentPart.getName();
-			if (currentPartName.equals(bPName)) 
+			String currentPartNumber = currentPart.getNumber();
+			if (currentPartNumber.equals(bPNumber)) 
 			{
 				 part = currentPart;
 				

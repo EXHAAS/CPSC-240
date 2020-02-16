c137f2f47efe3b11f51c8262fa375b52d54409ec
Anthony Tompkins <52788799+azt2504@users.noreply.github.com>
2020-02-16 07:04:09 -0500

    Added method to check if a valid input was entered.

--- EnterSellDisplay.java
+++ EnterSellDisplay.java
@@ -1,5 +1,5 @@
 /*THE METHOD TO ENTER A PART MANUALLY, SELL A PART, AND DISPLAY A PART
-*I hereby declare upon my word of honor that I have neither given nor recieved
+*I hereby declare upon my word of honor that I have neither given nor received
 *unauthorized help on this work
 *
 *@author Ellie Haas
@@ -9,36 +9,15 @@ import java.util.Scanner;
 import java.util.LinkedList;
 import java.util.Date;
 import java.text.SimpleDateFormat;
-
 public class EnterSellDisplay{
-   /*public static void main(String[] args){
-      Scanner reader = new Scanner(System.in);
-      
-      LinkedList<BikePart> tester = new LinkedList<BikePart>();
-      BikePart bp1 = new BikePart("WTB_saddle", 1234567890L, 33.00, 25.58, false, 5);
-      BikePart bp2 = new BikePart("26inTube", 1234567891L, 7.00, 5.58, true, 20);
-      BikePart bp3 = new BikePart("seatPost", 1234567892L, 17.00, 15.21, true, 300);
-      BikePart bp4 = new BikePart("carbonHandleBars42cm", 1234567893L, 47.00, 5.58, false, 8);
-      BikePart bp5 = new BikePart("frontTire", 1738495820L, 23.00, 20.00, false, 50);
-      BikePart bp6 = new BikePart("leftPedal", 1738597039L, 14.80, 23.00, true, 34);
-      BikePart bp7 = new BikePart("bikeRack", 2729475067L, 50.00, 45.00, true, 67);
-      
-      tester.add(bp1);
-      tester.add(bp2);
-      tester.add(bp3);
-      tester.add(bp4);
-      tester.add(bp5);
-      tester.add(bp6);
-      tester.add(bp7);
-      
-      enterPart(reader, tester);
-      sellPart(reader, tester);
-      //displayPart(reader, tester);
-      
-      for(BikePart part : tester){
-         part.printInfo();
-      }
-   }*/
+//   public static void main(String[] args){        
+//      enterPart(reader, tester);   
+//      sellPart(reader, tester);
+//      displayPart(reader, tester);     
+//      for(BikePart part : tester){
+//        part.printInfo();
+//       }
+//   }
    
 /*
 *Designed for the user to manually enter a part. Asks the user for the name and 
@@ -50,13 +29,23 @@ public class EnterSellDisplay{
 *@param s The Scanner passed in from the main method
 *@param l The inventory LinkedList that's searched/added to
 */
-   
-   public static void enterPart(Scanner s, LinkedList l){
+	
+	
+
+      public static void enterPart(Scanner s, LinkedList<BikePart> l){
       System.out.println("Enter the part's name:");
       String name = s.next();
       System.out.println("How many are there?");
-      int quantity = s.nextInt();
       
+      String quantityString = s.next();
+// When flag is equal to 1 the user input is checked to see if it is an integer.
+      int flag = 1;
+      String qInput = inputCheck(quantityString, flag);
+      if(qInput == null) {
+      System.out.println("Invalid Input \n\n");
+    	return;
+      }
+      int quantity = Integer.parseInt(quantityString);
       BikePart temp = searchBikePartList(l, name);
       if(temp != null){
          System.out.println("The part was already in the system.");
@@ -64,13 +53,31 @@ public class EnterSellDisplay{
          temp.setQuantity(var1 + quantity);
       }else{            
          System.out.println("Enter the part's number:");
-         long number = s.nextLong();
+         String number = s.next();
          System.out.println("Enter the part's list price:");
-         double lPrice = s.nextDouble();
+ // When flag is equal to 2 the user input is checked to see if it is an integer.        
+         flag = 2;
+         String lPriceString = s.next();
+         String lPInput = inputCheck(lPriceString, flag);
+         if(lPInput == null) {
+             System.out.println("Invalid Input \n\n");
+           	return;
+             }
+         double lPrice = Double.parseDouble(lPriceString);
          System.out.println("Enter the part's sales price:");
-         double sPrice = s.nextDouble();
+         
+         
+         String sPriceString = s.next();
+         String sPInput = inputCheck(lPriceString, flag);
+         if(sPInput == null) {
+             System.out.println("Invalid Input \n\n");
+           	return;
+             }
+         double sPrice = Double.parseDouble(sPriceString);
          System.out.println("Is the part on sale? y/n");
+         
          String ans = s.next();
+        
          boolean sale;
             if(ans.equalsIgnoreCase("y")){
                sale = true;
@@ -86,7 +93,9 @@ public class EnterSellDisplay{
          l.add(part);
          System.out.println("The part has been sucessfully added!");
       }  
-   }
+      }
+      
+      
  
 /*
 *Designed to sell the user a part. The user inputs the number of the part, then the
@@ -97,9 +106,9 @@ public class EnterSellDisplay{
 *@param s The Scanner passed in from the main method
 *@param l The inventory LinkedList being searched through
 */ 
-   public static void sellPart(Scanner s, LinkedList l){
+   public static void sellPart(Scanner s, LinkedList<BikePart> l){
       System.out.println("Enter the part's number: ");
-      long number = s.nextLong();
+      String number = s.next();
       
       BikePart temp = searchBikePartList(l, number);
       if(temp != null){
@@ -128,9 +137,9 @@ public class EnterSellDisplay{
 *system prints that it isn't there.
 *
 *@param s The Scanner passed in from the main method 
-*@param l The inventory LinkedList being searched throuh
+*@param l The inventory LinkedList being searched through
 */
-   public static void displayPart(Scanner s, LinkedList l){
+   public static void displayPart(Scanner s, LinkedList<BikePart> l){
       System.out.println("Enter the name of the part:");
       String name = s.next();
       
@@ -147,10 +156,10 @@ public class EnterSellDisplay{
       }
    }   
    
-   /**
+/**
 * 
 * *CPSC-240
-*I hereby declare upon my word of honor that I have neither given nor recieved
+*I hereby declare upon my word of honor that I have neither given nor received
 *unauthorized help on this work.
 *
 *  @author  Anthony Tompkins
@@ -158,55 +167,76 @@ public class EnterSellDisplay{
 
 /**
  *
- *This is a method that takes a LinkedList of BikePart objects and a number corresponding to the number field of 
- *a BikePart in the given LinkedList. It iterates through the LinkedList and returns the BikePart whos's part number field 
+ *This is a method that takes a LinkedList of BikePart objects and a name or number corresponding to the name or number field of 
+ *a BikePart in the given LinkedList. It iterates through the LinkedList and returns the BikePart whos's part name or number field 
  *is equal to the given string.
  *
  * @param bPLinkedList This parameter is the LinkedList that is being searched through.
- * @param bPNumber This parameter is the part number of the BikePart that is being looked for.
+ * @param bPNameOrNumber This parameter is the part number or name of the BikePart that is being looked for.
  * @return Returns BikePart named "part".
  */
-	public static BikePart searchBikePartList(LinkedList<BikePart> bPLinkedList, long bPNumber)
+	public static BikePart searchBikePartList(LinkedList<BikePart> bPLinkedList, String bPNameOrNumber)
 	{
 		BikePart part = null;
  		
 		for (BikePart currentPart : bPLinkedList) 
 		
 		{
-			if (currentPart.getNumber() == bPNumber) 
+			if (currentPart.getName().equals(bPNameOrNumber)) 
 			{
 				 part = currentPart;
-				
 			}
-
-		}
-		return part;
-  } 
-/**
- *
- *This is a method that takes a LinkedList of BikePart objects and a name corresponding to the name field of 
- *a BikePart in the given LinkedList. It iterates through the LinkedList and returns the BikePart whos's part name field 
- *is equal to the given string.
- *
- * @param bPLinkedList This parameter is the LinkedList that is being searched through.
- * @param bPName This parameter is the part number of the BikePart that is being looked for.
- * @return Returns BikePart named "part".
- */
-	public static BikePart searchBikePartList(LinkedList<BikePart> bPLinkedList, String bPName)
-	{
-		BikePart part = null;
- 		
-		for (BikePart currentPart : bPLinkedList) 
-		
-		{
-			if (currentPart.getName().equals(bPName)) 
+			else if (currentPart.getNumber().equals(bPNameOrNumber)) 
 			{
 				 part = currentPart;
-				
 			}
 
 		}
 		return part;
   } 
   
+
+/**
+ * Method to check if user input is valid.
+ * @param userInput The user's input as a string.
+ * @param flag integer variable to tell if userInput should be treated as an integer or double. 1 = integer 2 = double
+ * @return returns string which can either point to null or have the value of userInput.
+ */
+	public static String inputCheck(String userInput, int flag) 
+	{
+// invalid variable keeps track if a character in userInput was found to not be in numberString. 
+		int invalid = 0;
+// input variable is a string that is returned.
+		String input = null;
+// numberString is a string that holds numbers.
+		String numberString = null;
+// if flag is equal to 1 the userInput is checked to see if it is possible to be converted into an integer, or a double if flag
+// equals 2.
+		if(flag == 1) 
+		{
+			numberString = "1234567890";
+		}
+		else if(flag == 2) 
+		{
+			numberString = "1234567890.";
+		}
+// A char[] of userInput is made.	
+		char[] inputCharArray = userInput.toCharArray();
+// Loop were if a character does not occur in numberString the test is set assigned the value of -1 and invalid is incremented by 1. 
+		for(char c: inputCharArray)
+		{
+			int test = numberString.indexOf(c);
+			if(test < 0) 
+			{
+			invalid++;
+			}
+		
+		}
+// input is assigned the value of userInput if invalid is equal to 0.
+		if(invalid == 0) 
+		{
+		input = userInput;
+		}
+		return input;
+	}
 }

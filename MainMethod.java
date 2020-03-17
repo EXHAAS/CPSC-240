9572f447388d6ffbd26930a0ba58e81bd3f7113f
Anthony Tompkins <52788799+azt2504@users.noreply.github.com>
2020-03-17 18:35:21 -0400

    Update MainMethod.java

--- MainMethod.java
+++ MainMethod.java
@@ -1,6 +1,6 @@
 /*THE MAIN METHOD FOR PROJECT 1
 *
-*I hereby declare upon my word of honor that I have neither given nor recieved
+*I hereby declare upon my word of honor that I have neither given nor received
 *unauthorized help on this work
 *
 *@author Ellie Haas
@@ -16,41 +16,46 @@ public class MainMethod{
    
 	public static void main(String[] args)throws IOException{
 	  String warehouseFileName = "warehouseDB.txt";
-	 LinkedList<BikePart> bikeLL = Importer.Import(warehouseFileName, 0);
-	   
+
+	  LinkedList<BikePart> bikeLL = Importer.Import(warehouseFileName, 0);
+	  LinkedList<BikePart> totalInvLL = Importer.Import("totalinventory.txt", 0);
+	  
 	  Scanner reader = new Scanner(System.in);
       boolean loop = true;
       
       while(loop == true){
-//PRINT THE USERS CHOICES, THEN LET THEM ENTER THEIR CHOICE
-         System.out.println("Please select your option from the following menu:");
-         System.out.println("Read: Read an inventory delivery file");
-         System.out.println("Enter: Enter a part");
-         System.out.println("Sell: Sell a part");
-         System.out.println("Display: Display a part");
-         System.out.println("SortName: Sort parts by part name");
-         System.out.println("SortNumber: Sort parts by part number");
-         System.out.println("CreateVan: Create a new van for the fleet");
-         System.out.println("MoveInventory: Move BikeParts around warehouses");
-         System.out.println("Quit:");
-         System.out.println("Enter your choice:");
-         
-         String choice = reader.next();
-         
-         LinkedList<BikePart> blank = new LinkedList<BikePart>();
-         
-//CALL WHICHEVER METHOD THE READER CHOSE. LOOP UNTIL QUIT IS CHOSEN        
+    	//PRINT THE USERS CHOICES, THEN LET THEM ENTER THEIR CHOICE
+          System.out.println("Please select your option from the following menu:");
+          System.out.println("Read: Read an inventory delivery file");
+          System.out.println("Enter: Enter a part");
+          System.out.println("Sell: Sell a part");
+          System.out.println("Display: Display a part");
+          System.out.println("SortName: Sort parts by part name");
+          System.out.println("SortNumber: Sort parts by part number");
+          System.out.println("CreateVan: Create a new van for the fleet");
+          System.out.println("MoveInventory: Move BikeParts around warehouses");
+          System.out.println("Quit:");
+          System.out.println("Enter your choice:");
+          
+          String choice = reader.next();
+          
+          LinkedList<BikePart> blank = new LinkedList<BikePart>();
+          
+ //CALL WHICHEVER METHOD THE READER CHOSE. LOOP UNTIL QUIT IS CHOSEN           
          switch(choice.toUpperCase()){
             case "READ":
-               bikeLL = ReadInventory.readInventory(bikeLL, reader);              
+               totalInvLL = ReadInventory.readInventory(bikeLL, totalInvLL,reader);              
+               Writer.Write("totalinventory.txt", totalInvLL);
                break;
 
             case "ENTER":
-               EnterSellDisplay.enterPart(reader);
-                break;                          
+               totalInvLL = EnterSellDisplay.enterPart(reader,totalInvLL);
+               Writer.Write("totalinventory.txt", totalInvLL);
+               break;                          
             
             case "SELL":
-               EnterSellDisplay.sellPart(reader);
+               totalInvLL = EnterSellDisplay.sellPart(reader,totalInvLL);
+               Writer.Write("totalinventory.txt", totalInvLL);
                break;
  
             case "DISPLAY":
@@ -66,42 +71,43 @@ public class MainMethod{
                break;
                
             case "CREATEVAN":
-               System.out.println("Enter the name of the new van:");
-               String nm = reader.next();
-               Writer.Write(nm, blank);
-               Fleet.FleetAdd(nm);
-               System.out.println("Your van has been created\n\n");
-               break;
-               
-            case "MOVEINVENTORY":
-               Fleet.printFleet();
-               System.out.println("warehouseDB.txt");
-               System.out.println("Which file would you like to move from?");
-               String moveFile = reader.next();
-               LinkedList<BikePart> move = Van.chooseParts(moveFile, reader);
+                System.out.println("Enter the name of the new van:");
+                String nm = reader.next();
+                Writer.Write(nm, blank);
+                Fleet.FleetAdd(nm);
+                System.out.println("Your van has been created\n\n");
+                break;
+                
+             case "MOVEINVENTORY":
+                Fleet.printFleet();
+                System.out.println("warehouseDB.txt");
+                System.out.println("Which file would you like to move from?");
+                String moveFile = reader.next();
+                LinkedList<BikePart> move = Van.chooseParts(moveFile, reader);
 
-               Fleet.printFleet();
-               System.out.println("warehouseDB.txt");
-               System.out.println("Which file would you like to move to?");
-               String toFile = reader.next();
-               
-               Writer.Write(toFile, move);
-               System.out.println("Your inventory has been moved\n\n");
-               break;
- 
-            case "QUIT":
-               System.out.println("Thank you!");
-               loop = false;
-               break;               
+                Fleet.printFleet();
+                System.out.println("warehouseDB.txt");
+                System.out.println("Which file would you like to move to?");
+                String toFile = reader.next();
+                
+                Writer.Write(toFile, move);
+                System.out.println("Your inventory has been moved\n\n");
+                break;
+  
+             case "QUIT":
+                System.out.println("Thank you!");
+                loop = false;
+                break;               
 
-            default:
-               //NONE OF THESE WERE CHOSEN
-               System.out.println("Please enter a valid command \n\n");
-               break;
+             default:
+                //NONE OF THESE WERE CHOSEN
+                System.out.println("Please enter a valid command \n\n");
+                break;
             }
          }         
       //USE THE WRITER CLASS TO WRITE THE LINKEDLIST TO A FILE     
-      //Writer.Write(warehouseFileName, bikeLL);
-      //reader.close();        
+      
+      
+      reader.close();        
    }
 }

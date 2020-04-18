3711bbb95015bfcbb725961817af40aa69b2de6b
EXHAAS <60334101+EXHAAS@users.noreply.github.com>
2020-04-18 11:36:36 -0400

    Add files via upload

---
+++ Employee.java
@@ -0,0 +1,61 @@
+/*The Employee class that all other types of workers inherit from.
+*I hereby declare upon my word of honor that I have neither given nor
+*recieved unauthorized help on this work
+*
+*@author Eleanor Haas
+*/
+
+public class Employee{
+   String firstName;
+   String lastName;
+   String email;
+   String username;
+   String password;
+   
+   public Employee(String fn, String ln, String email, String un,
+      String password){
+      firstName = fn;
+      lastName = ln;
+      this.email = email;
+      username = un;
+      this.password = password;
+   }
+   
+/*Returns the Employee's first name
+*@return The Employee's first name
+*/   
+   public String getFirstName(){
+      return firstName;
+   }
+ 
+/*Returns the Employee's last name
+*@return The Employee's last name
+*/     
+   public String getLastName(){
+      return lastName;
+   }
+
+/*Returns the Employee's email
+*@return The Employee's email
+*/      
+   public String getEmail(){
+      return this.email;
+   }
+
+/*Returns the Employee's username
+*@return The Employee's username
+*/      
+   public String getUsername(){
+      return username;
+   }
+
+/*Sets the Employee's username
+*@param un The username to be set
+*/   
+   public void setUsername(String un){
+      this.username = un;
+   }
+   
+//NO METHOD TO GET THE PASSWORD BECAUSE THEY SHOULDN'T HAVE THAT
+   
+}

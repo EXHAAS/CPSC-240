b00b35cdf67e86b65475acee75554641d2230750
DrDudelsack <60376714+DrDudelsack@users.noreply.github.com>
2020-04-19 09:48:47 -0400

    Add files via upload

---
+++ OfficeManager.java
@@ -0,0 +1,26 @@
+
+/**
+ * Write a description of class OfficeManager here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class OfficeManager
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    /**
+     * Constructor for objects of class OfficeManager
+     */
+    public static void OfficeManager()
+    {
+        // initialise instance variables
+        //x = 0;
+        
+        
+        
+    }
+
+    
+}

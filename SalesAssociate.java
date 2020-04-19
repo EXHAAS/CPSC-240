568ed1451f34ae10fde2fc8caa2ccbde24105800
Anthony Tompkins <52788799+azt2504@users.noreply.github.com>
2020-04-19 13:12:50 -0400

    Add files via upload

---
+++ SalesAssociate.java
@@ -0,0 +1,22 @@
+/**
+* 
+* *CPSC-240
+*I hereby declare upon my word of honor that I have neither given nor received
+*unauthorized help on this work.
+*
+*  @author  Anthony Tompkins
+*/
+public class SalesAssociate extends Employee{
+
+
+	private String van;
+	SalesAssociate(String firstName, String lastName, String email, String username, String password,String van) {
+		super(firstName, lastName, email, username, password);
+		this.van = van;
+		this.type = "SalesAssociate";
+	}
+	
+	 public String getVan() {
+		 return this.van;
+	 }
+}
---
+++ WarehouseManager.java
@@ -0,0 +1,16 @@
+/**
+* 
+* *CPSC-240
+*I hereby declare upon my word of honor that I have neither given nor received
+*unauthorized help on this work.
+*
+*  @author  Anthony Tompkins
+*/
+public class WarehouseManager extends Employee {
+
+	public WarehouseManager(String fn, String ln, String email, String un, String password) {
+		super(fn, ln, email, un, password);
+		this.type = "WarehouseManager";
+	}
+
+}

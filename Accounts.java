32336a6d3f0118515d0eb0d96f490f4b931b0749
DrDudelsack <60376714+DrDudelsack@users.noreply.github.com>
2020-04-19 09:47:59 -0400

    Accounts is used for saving and reading accounts

---
+++ Accounts.java
@@ -0,0 +1,41 @@
+
+/**
+ * Write a description of class Accounts here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Accounts
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    /**
+     * Constructor for objects of class Accounts
+     */
+    public static void Accounts()
+    {
+        // initialise instance variables
+        //x = 0;
+        
+        
+        
+        
+        
+        
+    }
+    
+    public static void examinePart()
+    {
+        // initialise instance variables
+        //x = 0;
+        String partName;
+        
+        
+        
+        
+        
+    }
+
+    
+}

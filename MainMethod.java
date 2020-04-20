623a59b8e0c707b4770bfb4fe2b58a27a2d05013
Anthony Tompkins <52788799+azt2504@users.noreply.github.com>
2020-04-19 23:19:48 -0400

    Update MainMethod.java

--- MainMethod.java
+++ MainMethod.java
@@ -24,17 +24,17 @@ public class MainMethod{
 	  
 	  Scanner reader = new Scanner(System.in);
       
-      // Boolean for keeping the program running
-      boolean running = true;
-      // Boolean to check if an account is signed in
+	  // Boolean for keeping the program running
+	  boolean running = true;
+	  // Boolean to check if an account is signed in
       boolean loggedIn = false;
       // Boolean for signing in.
       boolean again = true;
       
- /*     SalesAssociate x = new SalesAssociate("Bob","Bobby","email","user","pass","S.txt"); // Account for testing
-      
+    SalesAssociate x = new SalesAssociate("Bob","Bobby","email","user","pass","S.txt"); // Account for testing
+    SystemAdministrator s = new SystemAdministrator("Bob","Bobby","email","user2","pass2");  
     accounts.add(x);
- */       
+    accounts.add(s);   
       Employee ac = null;
       while(running == true) 
       {
@@ -54,28 +54,27 @@ public class MainMethod{
     	  ac = a;
     	  loggedIn =true;
     	  again = false;
+      }            
       }
-      
       if(!loggedIn) {
     	  System.out.println("Incorrect login details.");
       }
-      }  
+      
       }  
       while(loggedIn == true){
     	//PRINT THE USERS CHOICES, THEN LET THEM ENTER THEIR CHOICE
           System.out.println("\nhello " + ac.getFirstName() + " " + ac.getLastName()+ "\n");
     	  
-    	  if(ac.getType() == 1) 
-	  {
-        	
+    	  if(ac.getType() == 1) {
+   
     	  System.out.println("Please select your option from the following menu:");
-    	  System.out.println("Logout: Sign off");
+    	  System.out.println("Create: Create new account");
+          System.out.println("Logout: Sign off");
     	  System.out.println("Quit:");
     	  System.out.println("Enter your choice:");
     	  
     	  }
-    	  if(ac.getType() == 2) 
-	  {
+    	  if(ac.getType() == 2) {
         	  
     	  System.out.println("Please select your option from the following menu:");
     	  System.out.println("Display: Display a part");
@@ -86,8 +85,7 @@ public class MainMethod{
     	  System.out.println("Quit:");
     	  
     	  }
-    	  if(ac.getType() == 3) 
-	  {
+    	  if(ac.getType() == 3) {
     	  
     	  System.out.println("Please select your option from the following menu:");
     	  System.out.println("Read: Read an inventory delivery file");
@@ -95,21 +93,20 @@ public class MainMethod{
     	  System.out.println("SortName: Sort parts by part name");
           System.out.println("SortNumber: Sort parts by part number");
           System.out.println("Logout: Sign off");
-	  System.out.println("Quit:");
-	  System.out.println("Enter your choice:");
+	      System.out.println("Quit:");
+	      System.out.println("Enter your choice:");
 
     	  }
-	  if(ac.getType() == 4) 
-	  {
+		  if(ac.getType() == 4) {
 	      
-	  System.out.println("Please select your option from the following menu:");
-	  System.out.println("Sell: Sell parts");
-	  System.out.println("LoadInventory: Load new inventory");
-	  System.out.println("Logout: Sign off");
-	  System.out.println("Quit:");
-	  System.out.println("Enter your choice:");
+		  System.out.println("Please select your option from the following menu:");
+		  System.out.println("Sell: Sell parts");
+	      System.out.println("LoadInventory: Load new inventory");
+	      System.out.println("Logout: Sign off");
+	      System.out.println("Quit:");
+	      System.out.println("Enter your choice:");
 	      
-	  }
+		  }
 
           
           
@@ -122,7 +119,7 @@ public class MainMethod{
  //CALL WHICHEVER METHOD THE READER CHOSE. LOOP UNTIL QUIT IS CHOSEN           
          switch(choice.toUpperCase()){
             case "READ":  
-               if(ac.getType() == 3) 
+               if(ac.getType()==3) 
                {          	   
                totalInvLL = ReadInventory.readInventory(bikeLL, totalInvLL,reader);              
                Writer.Write("totalinventory.txt", totalInvLL);
@@ -135,7 +132,7 @@ public class MainMethod{
 	       break;
 
             case "SELL":
-               if(ac.getType() == 4) 
+               if(ac.getType()==4) 
                {
                totalInvLL = EnterSellDisplay.sellPart(reader,totalInvLL, ac);
                Writer.Write("totalinventory.txt", totalInvLL);
@@ -148,11 +145,18 @@ public class MainMethod{
                break;
  
             case "DISPLAY":
-               EnterSellDisplay.displayPart(reader);
+            	if(ac.getType()==3 || ac.getType()==2) 
+            	{
+            		EnterSellDisplay.displayPart(reader);
+            	}
+            	else 
+                {
+            		System.out.println("Invalid Command");
+                }
                break;
  	    
 	         case "SORTNAME":
-               if(ac.getType() == 3 || ac.getType() == 2) 
+               if(ac.getType()==3 || ac.getType()==2) 
                {
 	           SortNameNumber.sortNameNumber(choice, reader);
                }
@@ -163,7 +167,7 @@ public class MainMethod{
                break;
 
             case "SORTNUMBER":
-            	if(ac.getType() == 3 || ac.getType() == 2) 
+            	if(ac.getType()==3 || ac.getType()==2) 
                 {
  	           SortNameNumber.sortNameNumber(choice, reader);
                 }
@@ -173,16 +177,95 @@ public class MainMethod{
                 }
                break;
                
-            case "CREATEVAN":
-                System.out.println("Enter the name of the new van:");
-                String nm = reader.next();
-                Writer.Write(nm, blank);
-                Fleet.FleetAdd(nm);
-                System.out.println("Your van has been created\n\n");
-                break;
-                
+            case "CREATE":
+            	if(ac.getType()==1) 
+            	{
+            	System.out.println("Enter:\n1: New system admin\n2: New Office Manager\n3: New Warehouse Manager\n4: New Sales Associate");
+            	int na = reader.nextInt();
+            	if(na==1) 
+            	{
+            		
+            		System.out.println("Enter First Name:");
+            		String fn = reader.next();
+            		System.out.println("Enter Last Name:");
+            		String ln = reader.next();
+            		System.out.println("Enter Email:");
+            		String e = reader.next();
+            		System.out.println("Enter Username:");
+            		String u = reader.next();
+            		System.out.println("Enter Password:");
+            		String p = reader.next();
+            		accounts.add(new SystemAdministrator(fn,ln,e,u,p));
+            		System.out.println("Account Creation Successful");
+            		
+            	}
+            	else if(na==2) 
+            	{
+            		System.out.println("Enter First Name:");
+            		String fn = reader.next();
+            		System.out.println("Enter Last Name:");
+            		String ln = reader.next();
+            		System.out.println("Enter Email:");
+            		String e = reader.next();
+            		System.out.println("Enter Username:");
+            		String u = reader.next();
+            		System.out.println("Enter Password:");
+            		String p = reader.next();
+            		accounts.add(new OfficeManager(fn,ln,e,u,p));
+            		System.out.println("Account Creation Successful");
+            	}
+            	else if(na==3) 
+            	{
+            		System.out.println("Enter First Name:");
+            		String fn = reader.next();
+            		System.out.println("Enter Last Name:");
+            		String ln = reader.next();
+            		System.out.println("Enter Email:");
+            		String e = reader.next();
+            		System.out.println("Enter Username:");
+            		String u = reader.next();
+            		System.out.println("Enter Password:");
+            		String p = reader.next();
+            		accounts.add(new WarehouseManager(fn,ln,e,u,p));
+            		System.out.println("Account Creation Successful");
+            	}
+            	else if(na==4) 
+            	{
+            		
+            		System.out.println("Enter First Name:");
+            		String fn = reader.next();
+            		System.out.println("Enter Last Name:");
+            		String ln = reader.next();
+            		System.out.println("Enter Email:");
+            		String e = reader.next();
+            		System.out.println("Enter Username:");
+            		String u = reader.next();
+            		System.out.println("Enter Password:");
+            		String p = reader.next();
+            		System.out.println("Enter Van Name");
+            		String v = reader.next();
+            		
+            		accounts.add(new SalesAssociate(fn,ln,e,u,p,v));
+            		System.out.println("Account Creation Successful\n");
+            		Writer.Write(v, blank);
+                    Fleet.FleetAdd(v);
+                    System.out.println("New Van Established\n\n");
+            	}
+            	else 
+            	{
+            		System.out.println("Invalid Input");
+            	}
+            	}
+                  	
+            	else 
+            	{
+            		 System.out.println("Invalid Command");
+            	}
+         		
+            	break;
+            
              case "LOADINVENTORY":
-                if(ac.getType() == 4) 
+                if(ac.getType()==4) 
                 {
             	Fleet.printFleet();
                 System.out.println("warehouseDB.txt");

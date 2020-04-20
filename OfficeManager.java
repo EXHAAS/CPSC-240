d79b26feadba6a71bbc89c7a8cd9924f327025a4
DrDudelsack <60376714+DrDudelsack@users.noreply.github.com>
2020-04-19 21:16:51 -0400

    Add files via upload

--- OfficeManager.java
+++ OfficeManager.java
@@ -1,26 +1,40 @@
-
+import java.io.IOException;
+import java.util.Scanner;
+import java.io.IOException;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.util.LinkedList;
 /**
  * Write a description of class OfficeManager here.
  *
  * @author (your name)
  * @version (a version number or a date)
  */
-public class OfficeManager
+public class OfficeManager extends Employee 
 {
     // instance variables - replace the example below with your own
-    private int x;
+    public OfficeManager(String fn, String ln, String email, String un,
+      String password){
+      super(fn, ln, email, un, password);
+   }
+
+   
 
     /**
      * Constructor for objects of class OfficeManager
      */
-    public static void OfficeManager()
+    public static void OfficeManagerReadName(java.util.Scanner reader) throws IOException
     {
-        // initialise instance variables
-        //x = 0;
+        // Will Be in a loop until it 
+        
+        System.out.println ("What action would you like to take?");
+        EnterSellDisplay.displayPart(reader);
         
         
         
     }
+    
+
 
     
 }
